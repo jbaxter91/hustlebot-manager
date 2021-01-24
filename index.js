db307e092fa65dee237cf3de7ded352c177510f6
John Baxter <jbaxter@a-zdarts.com>
2021-01-24 11:56:56 -0800

    meow?

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import fs from "fs";
 import util from 'util';
 import cp from"child_process";
 import path from 'path';
+import env from 'dotenv';
+
+env.config();
 
 // Convert fs.readFile into Promise version of same    
 const readFile = util.promisify(fs.readFile);
@@ -13,10 +16,11 @@ let config = JSON.parse(rawdata);
 let emulatorDelay = config.emulatorDelay * 1000;
 let lastRun = config.lastRun;
 let profiles = profile.ProfileList();
-let profileIndex = 0;
+let profileIndex = null;
 let __dirname = path.resolve();
 
 console.log(`Last Run Profile: ${lastRun}`);
+console.log(`profiles: ${profiles}`);
 
 function WatchBot()
 {
@@ -27,7 +31,8 @@ function WatchBot()
             console.log("Bot Stopped");
             clearInterval(timer);
             handler.KillNox();
-            ChangeProfile();
+            console.clear();
+            Startup();
           } else {
             console.log("Bot Running");
           }
@@ -46,8 +51,9 @@ async function Startup()
         return;
     }
 
-    if(lastRun)
+    if(profileIndex == null)
     {
+        console.log("No Profile Index found");
         for (let i = 0; i < profiles.length; i++) {
             const element = profiles[i];
             if(element == lastRun)
@@ -57,8 +63,8 @@ async function Startup()
 
     await ChangeProfile();
 
-    //console.log(`Trying to launch emulator at ${__dirname}/profiles/${profiles[profileIndex]}/${profiles}.lnk`);
-    cp.exec(`${__dirname}/profiles/${profiles[profileIndex]}/${profiles}.lnk`);
+    console.log(`Trying to launch emulator at ${__dirname}/profiles/${profiles[profileIndex]}/${profiles[profileIndex]}.lnk`);
+    cp.exec(`${__dirname}/profiles/${profiles[profileIndex]}/${profiles[profileIndex]}.lnk`);
     
     console.log(`${__dirname}/bot/HustleCastleBOT.exe`);
 
@@ -73,7 +79,7 @@ async function Startup()
         {
             console.log("Watching Bot!");
             WatchBot();
-        },20000)
+        },10000)
         
     }, emulatorDelay);
     
@@ -89,20 +95,39 @@ function GetProfileConfig()
 
 
 async function ChangeProfile() {
-  if (profileIndex >= profiles.length - 1) {
+    console.log(`Profile Index to start: ${profileIndex}`);
+    console.log(`Profile Length ${profiles.length}`);
+    profileIndex++;
+    console.log(`Profile Index to after bump: ${profileIndex}`);
+  if (profileIndex > (profiles.length - 1)) {
+    console.log("Returning to 0");
     profileIndex = 0;
   }
+  console.log(profileIndex);
+
+  console.log(`Changing Profile`);
+
+  await SetLicense();
 
   //console.log(profiles);
-  //console.log(profiles[profileIndex]);
+  console.log(`Getting config for profile: ${profiles[profileIndex]}`);
 
   GetProfileConfig()
   .then((res) => {
-    console.log("response",res);
+    //console.log("response",res);
     fs.writeFile("./bot/config.txt", res, function (err) {
         if (err) return console.log(err);
       });
-  });
 
-  
+        config.lastRun = profiles[profileIndex];
+        let data = JSON.stringify(config);
+        fs.writeFileSync('config.json', data);
+  });
+}
+
+async function SetLicense()
+{
+    fs.writeFile("./bot/license.txt", `email = ${process.env.HUSTLEBOT_EMAIL}\nkey =${process.env.HUSTLEBOT_KEY}`, function (err) {
+        if (err) return console.log(err);
+      });
 }
